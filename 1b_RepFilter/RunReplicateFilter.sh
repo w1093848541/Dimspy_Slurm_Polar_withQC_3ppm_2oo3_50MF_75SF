87867bde3615f51d443830e1432ced186342e67f
w1093848541 <77172096+w1093848541@users.noreply.github.com>
2022-02-08 21:36:20 -0500

    Update RunReplicateFilter.sh

--- 1b_RepFilter/RunReplicateFilter.sh
+++ 1b_RepFilter/RunReplicateFilter.sh
@@ -27,10 +27,10 @@ dimspy replicate-filter \
 --input ../1a_ProcessScans/process_scans.out \
 --output RepFilter.$SLURM_JOB_ID \
 --ppm $PPM \
---replicates $REPLICATES \
+--replicates 3 \
 --min-peak-present 3 \
 --report $REPORT_DIR/RepFilterReport.$SLURM_JOB_ID \
---ncpus 3 \
+--ncpus $NCPUS
 
 #other options to consider: --rsd_threshold, --filelist, --report
 echo "Filtering complete, begin conversion of files"
